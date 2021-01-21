694875ed8fa1d70946e9a313f9c785752e94cc0e
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-21 14:58:34 -0800

    initial commit

---
+++ src/main/java/com/aaronshaver/weirdjson/WeirdjsonApplication.java
@@ -0,0 +1,13 @@
+package com.aaronshaver.weirdjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeirdjsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeirdjsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaronshaver/weirdjson/WeirdjsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaronshaver.weirdjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeirdjsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
