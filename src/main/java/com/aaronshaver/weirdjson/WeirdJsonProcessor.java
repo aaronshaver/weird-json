76a0ce7822c14fcdbc14ae995b8a7e8201db18a7
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-22 10:23:33 -0800

    add processor class with constructor

---
+++ src/main/java/com/aaronshaver/weirdjson/WeirdJsonProcessor.java
@@ -0,0 +1,10 @@
+package com.aaronshaver.weirdjson;
+
+public class WeirdJsonProcessor {
+
+    private String data;
+
+    public WeirdJsonProcessor(String data) {
+        this.data = data;
+    }
+}
--- src/test/java/com/aaronshaver/weirdjson/KittyEventsTests.java
+++ src/test/java/com/aaronshaver/weirdjson/KittyEventsTests.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class WeirdjsonApplicationTests {
+class KittyEventsTests {
 
 	@Test
 	void mapKittyEventsObjectToJsonString() throws JsonProcessingException {
